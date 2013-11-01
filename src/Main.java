d7c4438e3b17dd48eed223738fd4ce03826be577
Yong Bakos <ybakos@humanoriented.com>
2013-10-31 21:43:39 -0500

    Adding simple PApplet stub -- working Processing project.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+// Main.java
+// An example Main class to bootstrap a Processing PApplet.
 // Yong Bakos
-// An example Main class.
 
 import processing.core.*;
 
 public class Main {
 
+    private final static String[] OPTIONS = new String[] { "Sketch" }; // add "--present" for present mode
+    
     public static void main(String[] args) {
-        System.out.println("A sample Java project for Xcode");
-        
+        System.out.println("A sample Java/Processing project for Xcode.");
+        PApplet.main(OPTIONS);
     }
 
 }
-
