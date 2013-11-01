2bd18061e4a7a891b7fed8f587d2be21dc1af655
Yong Bakos <projects@humanoriented.com>
2013-10-31 20:40:45 -0500

    Adding working ant buildfile and Main stub. Working project config.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Yong Bakos
+// An example Main class.
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("A sample Java project for Xcode");
+        
+    }
+
+}
+
